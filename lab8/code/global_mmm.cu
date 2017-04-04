7a21bde15fb1affcf0f48ffd794b8d6fc80f6de4
jsburke <jsburke@bu.edu>
2017-04-04 08:28:56 -0400

    add matrix and timing functions

--- lab8/code/global_mmm.cu
+++ lab8/code/global_mmm.cu
@@ -8,6 +8,8 @@
 #define TOL    1e-5
 double CPS =   2.9e9;
 
+int LEN;		// to be defined via cmd args
+
 ////////////////////////////  CUDA RELATED  ////////////////////////////////////
 
 
@@ -32,4 +34,202 @@ __global__ void MMM_kernel(float* A, float* B, float* dst, int len)
 		int k;
 		for(k = 0; k < len; k++) dst[row * len + col] = A[row * len + k] * B[k * len + col];
 	}
+}
+
+//////////////////////////////  MATRIX  /////////////////////////////////////////
+
+float* matrix_create(int len);
+int    matrix_init(float* mat, int len);
+int    matrix_zero(float* mat, int len);
+int    matrix_copy(float* src, float* dst, int len);
+
+/////////////////  Time related  //////////////////////////////
+
+//rdtsc related
+typedef union {
+  unsigned long long int64;
+  struct {unsigned int lo, hi;} int32;
+} mcps_tctr;
+
+#define MCPS_RDTSC(cpu_c) __asm__ __volatile__ ("rdtsc" : \
+                     "=a" ((cpu_c).int32.lo), "=d"((cpu_c).int32.hi))
+
+int clock_gettime(clockid_t clk_id, struct timespec *tp);
+struct timespec diff(struct timespec start, struct timespec end);
+double ts_ms(struct timespec ts);
+struct timespec ts_diff(struct timespec start, struct timespec end);
+double measure_cps(void);
+
+////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////
+
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////  MATRIX IMPLEMENTATIONS  ////////////////////////////////////////
+float float_rand(float min, float max)
+{
+	float f = (float)random()/RAND_MAX;
+	return  min + f * (max - min);
+}
+
+
+float* matrix_create(int len)
+{
+	float* arr;
+	if(len > 0)
+	{
+		arr = (float*) calloc(len*len, sizeof(float));
+		if(!arr)
+		{
+			printf("\n\tFailed to allocate array\n");
+			return NULL;
+		}
+	}
+	else return NULL;
+
+	return arr;
+}
+
+int matrix_init(float* mat, int len)
+{
+	int len_sq, i;
+
+	if(len > 0)
+	{
+		len_sq = len * len;
+		for (i = 0; i < len_sq; i++)
+		{
+			mat[i] = float_rand(MINVAL, MAXVAL);
+		}
+		return 1;
+	}
+	printf("\nError in initializing matrix\n");
+	return 0;
+}
+
+int matrix_zero(float* mat, int len)
+{
+	int len_sq, i;
+
+	if(len > 0)
+	{
+		len_sq = len * len;
+		for(i = 0; i < len_sq; i++)
+		{
+			mat[i] = 0;
+		}
+		return 1;
+	}
+	printf("\nFailed to zero matrix\n");
+	return 0;
+}
+
+int   matrix_copy(float* src, float* dst, int len)
+{
+	int len_sq, i;
+
+	if(len > 0)
+	{
+		len_sq = len * len;
+		for(i = 0; i < len_sq; i++)
+		{
+			dst[i] = src[i];
+		}
+		return 1;
+	}
+	printf("\nFailed to copy matrix\n");
+	return 0;
+}
+
+/////////////////////////////  Timing related  ///////////////////////////////
+
+double ts_ms(struct timespec ts)
+{
+  return ((((double)(ts.tv_sec))*1.0e9) + ((double)(ts.tv_nsec)))/(1.0e6);
+}
+
+/* ---------------------------------------------------------------------------
+| Make the CPU busy, and measure CPS (cycles per second).
+|
+| Explanation:
+| If tests are very fast, they can run so quickly that the SpeedStep control
+| (in kernel and/or on-chip) doesn't notice in time, and the first few tests
+| might finish while the CPU is still in its sleep state (about 800 MHz,
+| judging from my measurements)
+|   A simple way to get around this is to run some kind of busy-loop that
+| forces the OS and/or CPU to notice it needs to go to full clock speed.
+| We print out the results of the computation so the loop won't get optimised
+| away.
+|
+| Copy this code into other programs as desired. It provides three entry
+| points:
+|
+| double ts_sec(ts): converts a timespec into seconds
+| timespec ts_diff(ts1, ts2): computes interval between two timespecs
+| measure_cps(): Does the busy loop and prints out measured CPS (cycles/sec)
+--------------------------------------------------------------------------- */
+
+struct timespec ts_diff(struct timespec start, struct timespec end)
+{
+  struct timespec temp;
+  if ((end.tv_nsec-start.tv_nsec)<0) {
+    temp.tv_sec = end.tv_sec-start.tv_sec-1;
+    temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
+  } else {
+    temp.tv_sec = end.tv_sec-start.tv_sec;
+    temp.tv_nsec = end.tv_nsec-start.tv_nsec;
+  }
+  return temp;
+}
+
+double measure_cps()
+{
+  struct timespec cal_start, cal_end;
+  mcps_tctr tsc_start, tsc_end;
+  double total_time;
+  double total_cycles;
+  /* We perform a chaotic iteration and print the result, to defeat
+     compiler optimisation */
+  double chaosC = -1.8464323952913974; double z = 0.0;
+  long int i, ilim, j;
+
+  /* Do it twice and throw away results from the first time; this ensures the
+   * OS and CPU will notice it's busy and set the clock speed. */
+  for(j=0; j<2; j++) {
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &cal_start);
+    MCPS_RDTSC(tsc_start);
+    ilim = 50*1000*1000;
+    for (i=0; i<ilim; i++)
+      z = z * z + chaosC;
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &cal_end);
+    MCPS_RDTSC(tsc_end);
+  }
+
+  total_time = ts_ms(ts_diff(cal_start, cal_end));
+  total_cycles = (double)(tsc_end.int64-tsc_start.int64);
+  CPS = total_cycles / total_time;
+  printf("z == %f, CPS == %g\n", z, CPS);
+
+  return CPS;
+}
+/* ---------------------------------------------------------------------------
+| End of measure_cps code
+--------------------------------------------------------------------------- */
+
+struct timespec diff(struct timespec start, struct timespec end)
+{
+  struct timespec temp;
+  if ((end.tv_nsec-start.tv_nsec)<0) {
+    temp.tv_sec = end.tv_sec-start.tv_sec-1;
+    temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
+  } else {
+    temp.tv_sec = end.tv_sec-start.tv_sec;
+    temp.tv_nsec = end.tv_nsec-start.tv_nsec;
+  }
+  return temp;
 }
